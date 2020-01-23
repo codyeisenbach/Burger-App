3743c6134095098b5d16c79839603b89f4cdf392
Cbach <ceisenbach@codys-mbp.home>
2020-01-22 21:04:10 -0600

    first commit, schema and early MVC

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA `burgers_db` ;
+
+USE `burgers_db` ;
+
+CREATE TABLE `burgers_db`.`burgers` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `burger_name` VARCHAR(45) NULL,
+  `devoured` TINYINT NULL,
+  PRIMARY KEY (`id`));
+
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Swiss Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Krabby Patty Deluxe', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
