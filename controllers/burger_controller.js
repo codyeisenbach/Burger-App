6240d5641dfaf02326fa920c8affcea018cd227f
Cbach <ceisenbach@codys-mbp.home>
2020-01-22 22:34:40 -0600

    third commit, controller done

---
+++ controllers/burger_controller.js
@@ -0,0 +1,41 @@
+var express = require ('express');
+var router = express.Router();
+
+var burger = require('../models/burger.js');
+
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/burgers", function(req, res) {
+  burger.insertOne([
+    "burger_name"
+  ], [
+    req.body.burger_name
+  ], function(data) {
+    res.redirect('/');
+  });
+});
+
+router.put("/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: true
+  }, condition, function(data) {
+    res.redirect('/');
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
